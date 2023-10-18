34cbb2d0eedfd2a76a6ca1fd877c7dd4057d48cb
domin <dominik.mirek@hotmail.com>
2023-10-18 22:19:06 +0200

    działa

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager2 viewPager2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager2 = findViewById(R.id.viewpager);
+
+        ArrayList<ViewPagerItem> viewPagerItemList = new ArrayList<>();
+        viewPagerItemList.add(new ViewPagerItem(R.drawable.obraz1,getString(R.string.kwiaty)));
+        viewPagerItemList.add(new ViewPagerItem(R.drawable.obraz2,getString(R.string.oko)));
+        viewPagerItemList.add(new ViewPagerItem(R.drawable.obraz4,getString(R.string.rzeka)));
+
+        ViewAdapter viewAdapter = new ViewAdapter(viewPagerItemList);
+        viewPager2.setAdapter(viewAdapter);
     }
 }
